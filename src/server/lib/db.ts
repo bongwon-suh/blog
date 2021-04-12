2db1119c5cf4697d11e3b96c302a4e1b752fb185
bongwon-suh <suhliebe@gmail.com>
2021-04-12 23:19:28 +0900

    add db

---
+++ src/server/lib/db.ts
@@ -0,0 +1,24 @@
+import mariadb from 'mariadb';
+
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '901008',
+    connectionLimit: 5
+});
+async function asyncFunction() {
+    let conn;
+    try {
+      conn = await pool.getConnection();
+      const rows = await conn.query("SELECT 1 as val");
+      console.log(rows); //[ {val: 1}, meta: ... ]
+      const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+      console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  
+    } catch (err) {
+      throw err;
+    } finally {
+      if (conn) return conn.end();
+    }
+  }
+  
