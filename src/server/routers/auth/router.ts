610d333ff7b63c095d16a6cd05d91716da468a09
bongwon-suh <suhliebe@gmail.com>
2021-04-13 00:10:39 +0900

    add router

--- src/server/routers/auth/router.ts
+++ src/server/routers/auth/router.ts
@@ -11,4 +11,11 @@ router.get('/auth/login', (req, res)=>{
     return res.render('auth/login.njk');
 });
 
+router.get('/auth/signup', (req, res)=>{
+    return res.render('auth/signup.njk');
+});
+router.post('/auth/signup', (req, res)=>{
+    console.log(req.body);
+});
+
 export default router;
