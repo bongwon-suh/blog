912fa99db4deb5e361cd4044000d9bfb7ac39474
bongwon-suh <suhliebe@gmail.com>
2021-04-20 23:44:02 +0900

    add db

--- src/server/lib/db.ts
+++ src/server/lib/db.ts
@@ -1,19 +1,22 @@
 import mariadb from 'mariadb';
+import * as fs from 'fs';
+import path from 'path'
+
+const SECRET = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'SECRET.json'), 'utf8'))
 
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '901008',
-    connectionLimit: 5
+  "host": SECRET.DATABASE.HOST,
+  "user": SECRET.DATABASE.USER,
+  "password": SECRET.DATABASE.PASSWORD, 
+  "database": SECRET.DATABASE.DATABASE
 });
-async function asyncFunction() {
+
+async function asyncFunction(query: string) {
     let conn;
     try {
       conn = await pool.getConnection();
-      const rows = await conn.query("SELECT 1 as val");
-      console.log(rows); //[ {val: 1}, meta: ... ]
-      const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-      console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+      const rows = await conn.query(query);
+      console.log(rows);
   
     } catch (err) {
       throw err;
@@ -21,4 +24,5 @@ async function asyncFunction() {
       if (conn) return conn.end();
     }
   }
-  
+
+  export default asyncFunction;
