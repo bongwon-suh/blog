ce50217e2ccc53afc836c32f25d5720d1aa40666
bongwon-suh <suhliebe@gmail.com>
2021-05-02 00:56:15 +0900

    add passport

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,62 @@
  * @file run server
  * @author Bongwon Suh<suhliebe@gmail.com>
  */
+
+interface User{
+    "id": number // exactly, unsigned int. database internal primary key.
+    "user_id": string // username. it is used to login by user.
+    "user_pwd": string // encrpyted password.
+    "registter_time": Date
+};
+
 import path from 'path';
-import express from 'express';
+import express, { query } from 'express';
+import * as njk from 'nunjucks';
+import session from 'express-session';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import { Router, json } from 'express';
 import common from './lib/common';
-import * as njk from 'nunjucks'
+import auth from './lib/auth';
 import router from './routers/router';
+import asyncFunction from './lib/db'
 
 const app = express();
+
+app.use(session({
+    secret: 'keyboard cat'
+}));
+
+
+const passport_options: LocalStrategy.IStrategyOptions = {
+    usernameField: 'user_id',
+    passwordField: 'password',
+    session: true
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const local_strategy = new LocalStrategy.Strategy(passport_options,(user_id, password, cb)=>{
+    console.log(user_id, password)
+    auth.checkUser(user_id, password)
+    .then((user)=>{
+        console.log(user)
+        return cb(null, user)
+    })
+    .catch()
+});
+
+passport.use(local_strategy);
+
+passport.serializeUser( (user, cb)=>{
+    cb(null, user);
+});
+
+passport.deserializeUser( (user: User, cb)=>{
+    cb(null, user);
+});
+
 
 // template engine
 njk.configure(path.join(common.projectDir, 'static', 'template'), {
