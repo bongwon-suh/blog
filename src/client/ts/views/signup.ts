98c6146d1d8a3edc110e5567822a40b6b6172d4c
bongwon-suh <suhliebe@gmail.com>
2021-05-08 00:05:36 +0900

    add signup page

--- src/client/ts/views/signup.ts
+++ src/client/ts/views/signup.ts
@@ -1,21 +1,31 @@
-import { html, render, TemplateResult } from 'lit-html';
+/**
+ * @file Signup
+ * @author Bongwon Suh<suhliebe@gmail.com>
+ */
+
+import { css, html } from 'lit';
+import { customElement, property } from 'lit/decorators';
 import  AbstractView from './AbstractView';
 import * as utils from '../lib/utils';
 
-export default class Signup extends AbstractView {
-    constructor(container: HTMLElement) {
-        super(container);
-        this.setTitle("Signup");
+@customElement('auth-signup')
+export default class SignUp extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("SignUp");
     }
-
-    public submitSignUp = (e: Event)=>{
+ 
+    /**
+     * Protected Metod
+     * @param e MouseEvent
+     */
+ 
+    protected submitSignUp = (e: Event)=>{
         const user_id = document.getElementById('user_id') as HTMLInputElement;
         const password = document.getElementById('password') as HTMLInputElement;
         const confirm_password = document.getElementById('confirm_password') as HTMLInputElement;
 
-        if (password.value != confirm_password.value) {
-            return window.alert("비밀번호가 일치하지 않습니다.");
-        }
+        if (password.value != confirm_password.value) return window.alert("비밀번호가 일치하지 않습니다.");
 
         const msg = {
             "url": '/auth/signup',
@@ -37,29 +47,38 @@ export default class Signup extends AbstractView {
         })
     }
 
-    renderHTML = ()=>{
-        return render(this.makeTemplate(), this.container);
-    }
-
-    makeTemplate = ()=>{
-        return html `
-                    <div class="signup">
-                        <h2 class="signup_title">Sign Up</h2>
-                        <form class="signup-form">
-                            <div class="signup-form-field">
-                                <input id="user_id" name="user_id" type="text" class="signup-form-field__input" placeholder="ID" required />
-                            </div>
-                            <div class="signup-form-field">
-                                <input id="password" name="user_pwd" type="password" class="signup-form-field__input" placeholder="Password" required />
-                            </div>
-                            <div class="signup-form-field">
-                                <input id="confirm_password" type="password" class="signup-form-field__input" placeholder="Confirm Password" required />
-                            </div>
-                            <div class="signup-form-field">
-                                <div id="signup" class="signup-form-field__button" @click=${this.submitSignUp}>Sign up</div>
-                            </div>
-                        </form>
-                    </div>
-                    `
+    /**
+     * make Stylesheet
+     */
+    static styles = css`
+        :host {
+            color: green;
+        }
+    `;
+     
+    /* 
+    * make Template
+    */
+    render() {
+       return html `
+                   <div class="signup">
+                       <h2 class="signup_title">Sign Up</h2>
+                       <form class="signup-form">
+                           <div class="signup-form-field">
+                               <input id="user_id" name="user_id" type="text" class="signup-form-field__input" placeholder="ID" required />
+                           </div>
+                           <div class="signup-form-field">
+                               <input id="password" name="user_pwd" type="password" class="signup-form-field__input" placeholder="Password" required />
+                           </div>
+                           <div class="signup-form-field">
+                               <input id="confirm_password" type="password" class="signup-form-field__input" placeholder="Confirm Password" required />
+                           </div>
+                           <div class="signup-form-field">
+                               <div id="signup" class="signup-form-field__button" @click=${this.submitSignUp}>Sign up</div>
+                           </div>
+                       </form>
+                   </div>
+                   `
     }
 }
+ 
