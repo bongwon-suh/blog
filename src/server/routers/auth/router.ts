9caad4d92dd0ce0dbfbc24dfb2df3fac6f24b276
bongwon-suh <suhliebe@gmail.com>
2021-05-11 23:30:51 +0900

    modify router

--- src/server/routers/auth/router.ts
+++ src/server/routers/auth/router.ts
@@ -9,7 +9,6 @@ import * as control from './control'
 const router = Router();
 
 router.post('/auth/signup', json(), control.signup);
-// router.post('/auth/login', json(), control.postLogin);
 router.post('/auth/login', json(), control.login);
 
 router.post('/auth/test', json(), control.test);
