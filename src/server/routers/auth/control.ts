c03347cfdfaa84a333c59868121c71fa50ee2348
bongwon-suh <suhliebe@gmail.com>
2021-05-11 23:30:41 +0900

    modify passport

--- src/server/routers/auth/control.ts
+++ src/server/routers/auth/control.ts
@@ -1,12 +1,7 @@
 import { Request, response, Response } from 'express';
 import passport from 'passport';
-import asyncFunction from '../../lib/db';
-import { generatePassword, checkUser } from '../../lib/auth';
-// import { ControlFunction, ControlResult } from "lib/interface";
-import LocalStrategy from 'passport-local';
+import { generatePassword } from '../../lib/auth';
 import { User } from '../../models/User';
-import { createConnection } from 'typeorm';
-import { getConnection, getRepository } from 'typeorm';
 
 export const signup = async function (req: Request, res: Response) {
 
@@ -31,53 +26,31 @@ export const signup = async function (req: Request, res: Response) {
     }
 }
 
-export const postLogin = function(req: Request, res: Response) {
+export const login = async function(req: Request, res: Response) {
+    const user_id = req.body.user_id;
+    const password = generatePassword(req.body.password);
     
-    const response_msg = {
-        "fail": false,
-        "msg": ""
+    const user = await User.findOne({user_id: user_id})
+    if(user){
+        if(password == user.password) {
+            passport.authenticate('local', (err, user, info)=>{
+                req.login(user, (err)=>{
+                    if(err) res.status(402).json('실패')
+                    else res.status(200).json('로그인 완료')
+                })
+            })(req, res);
+        }
+        else {
+            res.status(401).json('비밀번호가 일치하지 않습니다.')    
+        }
     }
-
-    const user_id = req.body.user_id
-    const passowrd = req.body.password
-    console.log(user_id, passowrd + "컨트롤함수 내부")
-
-    passport.authenticate('local', (err, user, info)=>{
-        console.log("here")       
-        checkUser(user_id, passowrd)
-        .then( (user)=>{
-            // Here User serialzed
-            req.login(user, (err)=>{
-                if(err) {
-                    response_msg.fail = true;
-                    response_msg.msg = '시스템 문제로 인해 로그인에 실패했습니다.'
-                } else {
-                    return res.json(response_msg);
-                }
-            });
-        })
-        .catch( (err)=>{
-            // Failed to login    
-            response_msg.fail = true;
-
-            if(err.errno == 45028) {
-                response_msg.msg = '데이터 서버와의 연결이 좋지 않습니다.'
-            }
-            else {
-                response_msg.msg = '로그인에 실패했습니다.'
-            }
-
-            console.log(response_msg);
-        
-            res.json(response_msg);
-        });
-        
-    })(req, res);
-    
-    return response_msg;
+    else {
+        res.status(404).json('회원가입을 진행하세요.')
+    }
 }
 
 export const test = async function (req: Request, res: Response) {
-    const username = await User.findOne({user_id: "spwsbw"})
-    console.log(username)
+    console.log("=========로그인 테스트========")
+    console.log(req.user)
+    console.log("=================")
 }
