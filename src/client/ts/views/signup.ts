ef9a64924d5276dd2c4b8e6f7d2e030eec416937
bongwon-suh <suhliebe@gmail.com>
2021-05-13 23:40:47 +0900

    add style

--- src/client/ts/views/signup.ts
+++ src/client/ts/views/signup.ts
@@ -21,9 +21,9 @@ export default class SignUp extends AbstractView {
      */
  
     protected submitSignUp = (e: Event)=>{
-        const user_id = document.getElementById('user_id') as HTMLInputElement;
-        const password = document.getElementById('password') as HTMLInputElement;
-        const confirm_password = document.getElementById('confirm_password') as HTMLInputElement;
+        const user_id = this.shadowRoot!.getElementById('user_id') as HTMLInputElement;
+        const password = this.shadowRoot!.getElementById('password') as HTMLInputElement;
+        const confirm_password = this.shadowRoot!.getElementById('confirm_password') as HTMLInputElement;
 
         if (password.value != confirm_password.value) return window.alert("비밀번호가 일치하지 않습니다.");
 
@@ -38,12 +38,17 @@ export default class SignUp extends AbstractView {
 
         utils.sendAPI('POST', msg)
         .then( (result)=>{
-            if (result.fail) {
-                return window.alert(result.msg);
+            console.log(result);
+            if (result.status != 200) {
+                return window.alert(result.responseText);
             } else {
-                window.alert(result.msg);
-                window.location.replace('/');
+                window.alert(result.responseText);
+                window.location.replace('/login');
             }
+        })
+        .catch((err)=>{
+            console.log(err)
+            window.alert(err.responseText);
         })
     }
 
@@ -53,6 +58,62 @@ export default class SignUp extends AbstractView {
     static styles = css`
         :host {
             color: green;
+        }
+        .signup {
+            font-family: 'Montserrat', sans-serif;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 430px;
+            height: 420px;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+            border-radius: 5px;
+            position: absolute;
+            top: 100px;
+            left: 40%;
+        }
+        .signup_title {
+            margin-bottom: 45px;
+            font-size: 1.5rem;
+            font-weight: 500;
+            line-height: 1em;
+            text-transform: uppercase;
+            color: #e8716d;
+            letter-spacing: .1rem;
+        }
+
+
+        .signup-form {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+        }
+        .signup-form-field {
+            margin: 20px 10px;
+        }
+        .signup-form-field__input {
+            width: 200px;
+            border: 0;
+            border-bottom: 1px solid gray;
+        }
+            
+        .signup-form-field__input:focus {
+            outline: none;
+            border-bottom: 1px solid #e8716d;;
+        }
+
+        .signup-form-field__button {
+            justify-content: flex-end;
+            background: #e8716d;
+            width: 100px;
+            height: 30px;
+            border-radius: 5px;
+            color: white;
+            text-align: center;
+            padding-top:10px
         }
     `;
      
