efd19e032458227d5e4b5a2f7fc095fe0b21192e
bongwon-suh <suhliebe@gmail.com>
2021-05-14 23:47:45 +0900

    add nav component

---
+++ src/client/ts/views/common/nav.ts
@@ -0,0 +1,80 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators';
+import { until } from 'lit/directives/until';
+import * as utils from '../../lib/utils';
+import { User } from '../../lib/interfaces';
+
+@customElement('my-navbar')
+class MyHeader extends LitElement {
+    protected user: any
+
+    constructor() {
+        super();
+    }
+
+    protected getUserInfo2(): Promise<User> {
+        return new Promise ((resolve, reject)=>{
+            const msg = {
+                "url": '/auth/user-info',
+                "data": {}
+            }
+            utils.sendAPI('GET', msg)
+            .then( (result)=>{
+                resolve(result)
+            })
+            .catch( (err)=>{
+                reject(err)
+            });
+        })
+    }
+
+    static styles = css`
+        
+        .nav {
+            display: flex;
+            justify-content: flex-end;
+            width: 100vw;
+            background: #222222;
+            box-shadow: 0 0 1rem rgba(0,0,0,.1);
+        }
+        .nav__link {
+            padding: 20px 20px 20px 20px;
+            color: #eeeeee;
+            font-weight: 500;
+            text-decoration: none;
+        }
+        .nav__link:hover{
+            background: rgba(255, 255, 255, 0.05);
+        }
+    `;
+
+    render() {
+        return html`
+                    ${until(
+                        this.getUserInfo2()
+                        .then((result)=>{
+                            if(result.responseText) {
+                                return html`
+                                        <nav class="nav">
+                                            <a href="/" class="nav__link" data-link>Dashboard</a>
+                                            <a href="/post" class="nav__link" data-link>Posts</a>
+                                            <a href="/settings" class="nav__link" data-link>Settings</a>
+                                            <a href="/settings" class="nav__link" data-link>글쓰기</a>
+                                        </nav>
+                                        `
+                            }
+                            else {
+                                return html`
+                                        <nav class="nav">
+                                            <a href="/" class="nav__link" data-link>Dashboard</a>
+                                            <a href="/post" class="nav__link" data-link>Posts</a>
+                                            <a href="/settings" class="nav__link" data-link>Settings</a>
+                                        </nav>
+                                        `
+                            }
+                            })
+                        )
+                    }
+                    `
+    }
+}
