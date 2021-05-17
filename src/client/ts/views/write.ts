2c1d12c55d872eb2da9f38aced2cf06224fc99d9
bongwon-suh <suhliebe@gmail.com>
2021-05-17 22:57:01 +0900

    add write

--- src/client/ts/views/write.ts
+++ src/client/ts/views/write.ts
@@ -16,6 +16,45 @@ export default class Write extends AbstractView {
         this.setTitle("Write");
     }
 
+    protected submit = (e: Event)=>{
+        const category = this.shadowRoot!.getElementById('Category') as HTMLSelectElement;
+        const title = this.shadowRoot!.getElementById('Title') as HTMLInputElement;
+        const content = this.shadowRoot!.getElementById('Content') as HTMLTextAreaElement;
+        let url = '';
+
+        switch(category.value) {
+            case 'Project':
+                url = '/project'
+                break;
+
+            case 'Blog':
+                url = '/blog'
+                break;
+        }
+
+        const msg = {
+            "url": url,
+            "data": {
+                "title": title.value,
+                "content": content.value
+            }
+        };
+
+        utils.sendAPI('POST', msg)
+        .then((result)=>{
+            if(result.status != 200) {
+                return window.alert(result.responseText);
+            } else {
+                window.alert(result.responseText);
+                window.location.replace('/');
+            }
+        })
+        .catch((err)=>{
+            window.alert(err.responseText);
+        })
+
+    }
+
     /**
      * make Stylesheet
      */
@@ -29,6 +68,25 @@ export default class Write extends AbstractView {
      * make Template
      */
     render() {
-        return html``
+        return html`
+                    <my-navbar></my-navbar>
+                    <form class="write-form">
+                        <div class="form-field">
+                            <select id="Category" class="form-field_select">
+                                <option value="Project">Project</option>
+                                <option value="Blog">Blog</option>
+                            </select>
+                        </div>
+                        <div class="form-field">
+                            <input id="Title" class="form-field_input" type="text" placeholder="title" required />
+                        </div>
+                        <div class="form-field">
+                            <textarea id="Content" class="form-field_textarea"></textarea>
+                        </div>
+                        <div class="form-field">
+                            <div id="submit" class="form-field_button" @click=${this.submit}>Login</div>
+                        </div>
+                    </form>
+                `
     }
 }
