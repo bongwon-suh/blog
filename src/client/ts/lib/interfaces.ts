0d6dea66c1c6628109ea5271401bc469cabf9c29
bongwon-suh <suhliebe@gmail.com>
2021-05-18 23:07:50 +0900

    add interface

--- src/client/ts/lib/interfaces.ts
+++ src/client/ts/lib/interfaces.ts
@@ -6,3 +6,11 @@ export interface ResponseStatus {
 export interface User extends ResponseStatus {
     "responseText": {id: number, user_id: string, password: string, register_date: Date}
 }
+
+export interface Project extends ResponseStatus {
+    "id": number;
+    "title": string;
+    "content": string;
+    "register_date": Date;
+    "authorId": number;
+}
