494d14eb05e94856fdabaf874fcbc8eb007c3785
bongwon-suh <suhliebe@gmail.com>
2021-05-19 21:00:13 +0900

    add project list

--- src/client/ts/views/project.ts
+++ src/client/ts/views/project.ts
@@ -4,58 +4,60 @@
  * @author Bongwon Suh<suhliebe@gmail.com>
  */
 
- import { css, html } from 'lit';
- import { customElement, property } from 'lit/decorators';
- import { until } from 'lit/directives/until';
- import  AbstractView from './AbstractView';
- import * as utils from '../lib/utils';
- import { Project } from '../lib/interfaces';
- 
- @customElement('project-container')
- export default class Setting extends AbstractView {
-     constructor() {
-         super();
-         this.setTitle("Project");
-     }
- 
-     protected getProjectList(): Promise<Project[]> {
-         return new Promise( (resolve, reject)=>{
-             const msg = {
-                 "url": '/project',
-                 "data": {}
-             };
-             utils.sendAPI('GET', msg)
-             .then( (result)=>{
-                 resolve(result)
-             })
-             .catch((err)=>{
-                 reject(err)
-             });
-         })
-     }
- 
-     /**
-      * make Stylesheet
-      */
-     static styles = css`
-         :host {
-             color: blue;
-         }
-     `;
-     
-     @property()
-     name?: string = 'world';
- 
-     /**
-      * make Template
-      */
-     render() {
-         this.getProjectList()
-         .then((result)=>{
-             
-         })
-         return html`
-                     `;
-     }
- }
- 
+import { css, html } from 'lit';
+import { customElement, property } from 'lit/decorators';
+import { until } from 'lit/directives/until';
+import  AbstractView from './AbstractView';
+import * as utils from '../lib/utils';
+import { Project } from '../lib/interfaces';
+
+@customElement('project-container')
+export default class Setting extends AbstractView {
+    protected list: Array<Project>
+    constructor() {
+        super();
+        this.setTitle("Project");
+        this.getProjectList();
+        this.list = []
+    }
+
+    protected getProjectList(): Promise<Project[]> {
+        return new Promise( (resolve, reject)=>{
+            const msg = {
+                "url": '/project',
+                "data": {}
+            };
+            utils.sendAPI('GET', msg)
+            .then( (result)=>{
+                console.log(result)
+                resolve(result)
+            })
+            .catch((err)=>{
+                console.log(err)
+                reject(err)
+            });
+        })
+    }
+
+    /**
+     * make Stylesheet
+     */
+    static styles = css`
+        :host {
+            color: blue;
+        }
+    `;
+    
+    @property()
+    name?: string = 'world';
+    // ${until(this.getProjectList().then(result=>result.map(i=>html`${i.id}`)))}
+    /**
+     * make Template
+     */
+    render() {
+        return html`
+                <my-navbar></my-navbar>
+                ${until(this.getProjectList().then(result=>result.map(i=>html`${i.id}`)))}
+                    `;
+    }
+}
