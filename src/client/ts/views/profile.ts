37368d0814c2dbfaba1e63a9759799a1e33e1028
bongwon-suh <suhliebe@gmail.com>
2021-05-23 02:40:26 +0900

    modify profile

--- src/client/ts/views/profile.ts
+++ src/client/ts/views/profile.ts
@@ -21,12 +21,18 @@ export default class Profile extends AbstractView {
      * make Stylesheet
      */
     static styles = css`
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
         .profile {
             display: flex;
             justify-content: center;
+            width: 100vw;
             background: #222222;
             clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 100px));
-            padding: 200px 0;
+            padding: 100px 0 150px 0;
         }
         .profile-text {
             display: flex;
@@ -34,14 +40,16 @@ export default class Profile extends AbstractView {
             margin-right: 50px;
         }
         .profile-text_title {
-            font-size: 40px;
+            font-size: 36px;
             font-weight: 700;
             color: white;
+            margin-bottom: 20px;
         }
         .profile-text_subtitle {
             font-size: 28px;
-            width: 400px;
+            width: 500px;
             color: #ABABAB;
+            line-height: initial;
         }
         .profile-text_button {
             text-decoration: none;
@@ -51,15 +59,75 @@ export default class Profile extends AbstractView {
             font-size: 18px;
             padding: 10px 20px;
             border-radius: 10px;
+            margin-top: 30px;
         }
         .profile-image {
-            padding: 10px;
-            background: black;
-            margin-left:50px;
+            padding: 40px;
+            background: #121316;
+            border-radius: 5px;
         }
         .profile-image_img {
-            width: 250px;
-            border-radius: 5px;
+            width: 300px;
+            border-radius: 3px;
+            margin-bottom: -80px;
+        }
+        .skill {
+            display: flex;
+            flex-direction: column;
+            width: 90%;
+            margin: 50px 0;
+        }
+        .skill-title_text {
+            font-size: 24px;
+            color: #919191;
+            font-weight: 600;
+            border-bottom: 4px solid #f6f6f6;
+            letter-spacing: .063em;
+            padding-bottom: 15px;
+        }
+        .skill-content {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .skill-content-image {
+            width: 200px;
+            margin: 30px 75px 30px 0;
+        }
+        .skill-content_img {
+            width: 60px;
+        }
+        .skill-content-descripition {
+            width: 500px;
+            margin-left: 30px 0 30px; 75px;
+        }
+        .skill-content_text {
+            width: 500px;
+        }
+        .life {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin: 50px 0;
+            width: 100vw;
+            height: 800px;
+            clip-path: polygon(0 0, 100% 100px, 100% 100%, 0 calc(100% - 100px));
+            background: #f6f6f6;
+            padding-bottom: 250px;
+        }
+        .life-title {
+            margin-top: 80px;
+        }
+        .life-title_text {
+            font-size: 24px;
+            color: #919191;
+            font-weight: 600;
+            border-bottom: 4px solid;
+            letter-spacing: .063em;
+            padding-bottom: 15px;
+        }
+        .life_img {
+            width: 800px;
         }
     `;
 
@@ -69,14 +137,67 @@ export default class Profile extends AbstractView {
     render() {
         return html`
                 <my-navbar></my-navbar>
-                <div class="profile">
-                    <div class="profile-text">
-                        <span class="profile-text_title">Lorem 한글은 어떻게 나오나 is simply</span>
-                        <span class="profile-text_subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</span>
-                        <a class="profile-text_button" href="/projects">My Projects</a>
+                <div class="container">
+                    <div class="profile">
+                        <div class="profile-text">
+                            <span class="profile-text_title">Lorem 한글은 어떻게 나오나 is simply</span>
+                            <span class="profile-text_subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</span>
+                            <a class="profile-text_button" href="/projects">My Projects</a>
+                        </div>
+                        <div class="profile-image">
+                            <img class="profile-image_img" src="/static/images/profile2.jpg">
+                        </div>
                     </div>
-                    <div class="profile-image">
-                        <img class="profile-image_img" src="/static/images/profile.jpg">
+                    <div class="skill">
+                        <div class="skill-title">
+                            <p class="skill-title_text">SKILLS</p>
+                        </div>
+                        <div class="skill-content">
+                            <div class="skill-content-image">
+                                <img class="skill-content_img" src="/static/images/typescript.png">
+                                <img class="skill-content_img" src="/static/images/javascript.png">
+                                <img class="skill-content_img" src="/static/images/nodejs.png">
+                            </div>
+                            <div class="skill-content-descripition">
+                                <span class="skill-content_text">Lorem Ipsum is simply dummy text of the printing</span>
+                            </div>
+                        </div>
+                        <div class="skill-content">
+                            <div class="skill-content-image">
+                                <img class="skill-content_img" src="/static/images/python.png">
+                                <img class="skill-content_img" src="/static/images/django.png">
+                                <img class="skill-content_img" src="/static/images/flask.png">
+                            </div>
+                            <div class="skill-content-descripition">
+                                <span class="skill-content_text">Lorem Ipsum is simply dummy text of the printing</span>
+                            </div>
+                        </div>
+                        <div class="skill-content">
+                            <div class="skill-content-image">
+                                <img class="skill-content_img" src="/static/images/mariadb.png">
+                                <img class="skill-content_img" src="/static/images/mongodb.png">
+                            </div>
+                            <div class="skill-content-descripition">
+                                <span class="skill-content_text">Lorem Ipsum is simply dummy text of the printing</span>
+                            </div>
+                        </div>
+                        <div class="skill-content">
+                            <div class="skill-content-image">
+                                <img class="skill-content_img" src="/static/images/html.png">
+                                <img class="skill-content_img" src="/static/images/css.png">
+                            </div>
+                            <div class="skill-content-descripition">
+                                <span class="skill-content_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="life">
+                        <div class="life-title">
+                            <p class="life-title_text">LIFE</p>
+                        </div>
+                        <div class="life-image">
+                            <img class="life_img" src="/static/images/life.png">
+                        </div>
                     </div>
                 </div>
                 `
