2002b4e66bc9b1985a88113b6ec3c2f7cd18e33f
bongwon-suh <suhliebe@gmail.com>
2021-05-27 23:16:43 +0900

    add model

---
+++ src/server/models/Blog.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, CreateDateColumn, ManyToOne } from 'typeorm';
+import { User } from "./User";
+
+@Entity()
+export class Blog extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column({type: "text"})
+    content: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    dev_period: string;
+
+    @Column()
+    dev_tool: string;
+
+    @CreateDateColumn()
+    register_date: Date;
+
+    @ManyToOne(()=> User, user => user.projects)
+    author: User;
+}
