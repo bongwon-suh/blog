2d645185d5111877e79554c5daa0c3dab067b633
bongwon-suh <suhliebe@gmail.com>
2021-06-01 23:29:07 +0900

    seperate routes

--- src/client/ts/router.ts
+++ src/client/ts/router.ts
@@ -5,18 +5,21 @@
 
 import { Router } from '@vaadin/router';
 
+export const routes = [
+    { path: "/", component: "profile-container" },
+    { path: "/projects", component: "project-container" },
+    { path: "/post", component: "post-container"},
+    { path: "/post/:id", component: "post-detail"},
+    { path: "/login", component: "auth-login" },
+    { path: "/signup", component: "auth-signup" },
+    { path: "/write", component: "write-container"},
+]
+
 window.addEventListener("load", ()=>{
     initRouter();
 })
 
 function initRouter() {
     const router = new Router(document.querySelector("main"));
-    router.setRoutes([
-        { path: "/", component: "profile-container" },
-        { path: "/projects", component: "project-container" },
-        { path: "/post", component: "post-container"},
-        { path: "/login", component: "auth-login" },
-        { path: "/signup", component: "auth-signup" },
-        { path: "/write", component: "write-container"},
-    ])
+    router.setRoutes(routes)
 }
