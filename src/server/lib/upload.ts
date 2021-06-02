67ac3dfb45de88154e838f5ea98107544f709da0
bongwon-suh <suhliebe@gmail.com>
2021-06-03 00:08:00 +0900

    add lib

---
+++ src/server/lib/upload.ts
@@ -0,0 +1,13 @@
+import multer from 'multer';
+import path from 'path';
+
+export const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename: function (req, file, cb) {
+            cb(null, new Date().valueOf() + path.extname(file.originalname));
+        }
+    }),
+});
