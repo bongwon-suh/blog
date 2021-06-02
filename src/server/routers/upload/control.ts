ea0d6a440ca466ca5ad2a3bf236dfd2574168468
bongwon-suh <suhliebe@gmail.com>
2021-06-03 00:07:43 +0900

    update uploadFile fn

--- src/server/routers/upload/control.ts
+++ src/server/routers/upload/control.ts
@@ -1,9 +1,29 @@
-import { Request, response, Response } from 'express';
-import { User } from '../../models/User';
-import { ResUser } from '../../lib/interfaces'
-import { Project } from '../../models/Project';
+import { Request, Response } from 'express';
+import { ImageFile } from '../../lib/interfaces';
+import { File } from '../../models/File';
 
 export const uploadFile = async function(req: Request, res: Response) {
-    console.log(req.body);
-    console.log(req.files);
+    const file: ImageFile = req.file;
+
+    if(!file.mimetype.includes("image")) {
+        return res.status(500).json("이미지 파일이 아닙니다.");
+    }
+
+    try {
+        const upload = new File();
+        upload.destination = file.destination;
+        upload.encoding = file.destination;
+        upload.fieldname = file.fieldname;
+        upload.filename = file.filename;
+        upload.mimetype=file.mimetype
+        upload.originalname=file.originalname
+        upload.path=file.path
+        upload.size=file.size
+
+        await upload.save();
+        res.status(200).json("등록을 완료했습니다.")
+    }
+    catch {
+        return res.status(500).json("오류가 발생했습니다.");
+    }
 }
--- src/server/routers/upload/router.ts
+++ src/server/routers/upload/router.ts
@@ -1,10 +1,9 @@
 import { Router, json } from 'express';
 import * as control from './control';
-import multer from 'multer';
+import { upload } from '../../lib/upload';
 
 const router = Router();
-const upload = multer({ dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 } });
 
-router.post('/upload', upload.single('img'), control.uploadFile);
+router.post('/upload', upload.single('img'), control.uploadFile)
 
 export default router
