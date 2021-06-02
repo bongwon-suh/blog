03ebc980dcf08d78cd2f0737d7e3f56f35334682
bongwon-suh <suhliebe@gmail.com>
2021-06-03 00:06:48 +0900

    add imagefile interface

--- src/server/lib/interfaces.ts
+++ src/server/lib/interfaces.ts
@@ -4,3 +4,14 @@ export interface ResUser {
     "password": string;
     "regitser_date": Date;
 }
+
+export interface ImageFile {
+    "fieldname": string;
+    "originalname": string;
+    "encoding": string;
+    "mimetype": string;
+    "destination": string;
+    "filename": string;
+    "path": string;
+    "size": number;
+}
