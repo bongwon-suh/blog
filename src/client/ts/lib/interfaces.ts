bda7469e72e85f6ba8bee8adc135fcfd7a1cad8f
bongwon-suh <suhliebe@gmail.com>
2021-06-06 01:00:29 +0900

    modify interface

--- src/client/ts/lib/interfaces.ts
+++ src/client/ts/lib/interfaces.ts
@@ -1,45 +1,41 @@
 export interface ResponseStatus {
-    "status": number;
-    "statusText": string;
+    status: number;
+    statusText: string;
 }
-
-// export interface User extends ResponseStatus {
-//     "responseText": {id: number, user_id: string, password: string, register_date: Date}
-// }
 
 export interface User {
-    "id": number;
+    id: number;
 }
 
-export interface Project extends ResponseStatus {
-    "id": number;
-    "title": string;
-    "content": string;
-    "register_date": Date;
-    "authorId": number;
-    "description": string;
-    "dev_period": string;
-    "dev_tool": string;
+export interface Project {
+    id?: number;
+    title?: string;
+    content: string;
+    register_date?: Date;
+    authorId?: number;
+    description?: string;
+    dev_period?: string;
+    dev_tool?: string;
 }
 
 export interface Blog {
-    "id": number;
-    "title": string;
-    "content": string;
-    "register_date": Date;
-    "authorId": number;
-    "description": string;
-    "dev_period": string;
-    "dev_tool": string;
+    id?: number;
+    title?: string;
+    content: string;
+    register_date?: Date;
+    authorId?: number;
+    description?: string;
+    dev_period?: string;
+    dev_tool?: string;
 }
 
 export interface ImageFile {
-    "fieldname": string;
-    "originalname": string;
-    "encoding": string;
-    "mimetype": string;
-    "destination": string;
-    "filename": string;
-    "path": string;
-    "size": number;
+    fieldname: string;
+    originalname: string;
+    encoding: string;
+    mimetype: string;
+    destination: string;
+    filename: string;
+    path: string;
+    size: number;
 }
