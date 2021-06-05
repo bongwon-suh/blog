515241f17ec546a79444ce39855c9e80488b527d
bongwon-suh <suhliebe@gmail.com>
2021-06-06 01:04:35 +0900

    post detail

--- src/client/ts/views/post_detail.ts
+++ src/client/ts/views/post_detail.ts
@@ -1,61 +1,102 @@
 /**
  * @file Post detail page
  * @todo will be change
- * @author Bongwon Suh<suhliebe@gmail.com> 3
+ * @author Bongwon Suh<suhliebe@gmail.com> 4
  */
 
- import { css, html } from 'lit';
- import { customElement, property } from 'lit/decorators';
- import  AbstractView from './AbstractView';
- import { sendAPI, parseParams} from '../lib/utils';
- import { until } from 'lit/directives/until';
- import { unsafeHTML } from 'lit/directives/unsafe-html';
- import { Blog } from '../lib/interfaces';
- import { routes } from '../router';
- 
+import { css, html } from 'lit';
+import { customElement, property } from 'lit/decorators';
+import  AbstractView from './AbstractView';
+import { sendAPI, parseParams} from '../lib/utils';
+import { until } from 'lit/directives/until';
+import { unsafeHTML } from 'lit/directives/unsafe-html';
+import { Blog } from '../lib/interfaces';
+import { routes } from '../router';
 
- @customElement('post-detail')
- export default class PostDetail extends AbstractView {
-     constructor() {
-         super();
-         this.setTitle("Blog");
-     }
- 
-     protected getBlogDetail(): Promise<Blog> {
-         return new Promise( (resolve, reject)=>{
-             const param = parseParams(routes[3]['path'], location.pathname);
-             console.log(param.id);
-             const msg = {
-                 "url": `/blog/${param.id}`,
-                 "data": {}
-             };
-             sendAPI('GET', msg)
-             .then( (result)=>{
-                 console.log(result)
-                 resolve(result)
-             })
-             .catch((err)=>{
-                 console.log(err)
-                 reject(err)
-             });
-         })
-     }
- 
-     /**
-      * make Stylesheet
-      */
-     static styles = css`
-     `;
- 
-     /**
-      * make Template
-      */
-     render() {
-         return html`
-                     <my-navbar></my-navbar>
-                     <h1>hello world</h1>
-                     ${until(this.getBlogDetail().then(result=>html`<div>${unsafeHTML(result.title)}</div>`))}
-                     `
-     }
- }
- 
+
+@customElement('post-detail')
+export default class PostDetail extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("Blog");
+        this.setProperties();
+    }
+
+    @property( { type: Object })
+    items: Blog = { content: ""};
+
+    protected setProperties = () => {
+        this.getBlogDetail()
+        .then((result)=>{
+            this.items = result;
+        })
+    }
+
+    protected getBlogDetail(): Promise<Blog> {
+        return new Promise( (resolve, reject)=>{
+            const param = parseParams(routes[4]['path'], location.pathname);
+            const msg = {
+                "url": `/blog/${param.id}`,
+                "data": {}
+            };
+            sendAPI('GET', msg)
+            .then( (result)=>{
+                resolve(result);
+            })
+            .catch((err)=>{
+                reject(err);
+            });
+        })
+    }
+
+    /**
+     * make Stylesheet
+     */
+    static styles = css`
+        .top {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+            background: #222222;
+            clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 100px));
+            padding: 100px 0 150px 0;
+        }
+        .top-text {
+            display: flex;
+            flex-direction: column;
+            margin-right: 50px;
+        }
+        .top-text_title {
+            text-align: center;
+            font-size: 36px;
+            font-weight: 700;
+            color: white;
+            margin-bottom: 20px;
+        }
+        .top-text_subtitle {
+            font-size: 28px;
+            width: 500px;
+            color: #ABABAB;
+            line-height: initial;
+        }
+    `;
+
+    /**
+     * make Template
+     */
+    render() {
+        return html`
+                    <my-navbar></my-navbar>
+                    <section class="top">
+                        <div class="top-text">
+                            <span class="top-text_title">${this.items.title}</span>
+                            <span class="top-text_subtitle">${this.items.description}</span>
+                        </div>
+                    </section>
+                    <section class="content">
+                        ${unsafeHTML(this.items.content)}
+                    </section>
+                    <my-footer></my-footer>
+                    `
+    }
+}
