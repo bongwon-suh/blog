c5ea4c8d802e8b5819406a98de587b00712ebb28
bongwon-suh <suhliebe@gmail.com>
2021-06-06 00:57:54 +0900

    add getprojectdetail fn

--- src/server/routers/project/router.ts
+++ src/server/routers/project/router.ts
@@ -4,6 +4,8 @@ import * as control from './control'
 const router = Router();
 
 router.post('/project', json(), control.createProject);
+
 router.get('/project', json(), control.getProjectList);
+router.get('/project/:id', json(), control.getProjectDetail);
 
 export default router
